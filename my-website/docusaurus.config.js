06657e208bf182c50b25a57cd1ff36bcd36a69b2
Alexander Lazaris <alex.lazaris617@gmail.com>
2023-08-22 23:04:38 +1000

    github pages config change

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'My home wiki',
+  tagline: 'This is very handy',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -18,9 +18,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'alexandlazaris', // Usually your GitHub org/user name.
+  projectName: 'home-wiki', // Usually your repo name.
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
